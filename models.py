dcdc091227071ce229023e0bcf825e1a3e8c1281
adiyatmubarak <adiyatmubarak@gmail.com>
2016-06-23 00:17:52 +0700

    feature: initial model schema

---
+++ models.py
@@ -0,0 +1,56 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.db.models import PROTECT
+from django.utils.encoding import python_2_unicode_compatible
+
+
+@python_2_unicode_compatible
+class Province(models.Model):
+    name = models.CharField(max_length=255)
+    province_id = models.CharField(max_length=80, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, **kwargs):
+        raise NotImplementedError
+
+
+@python_2_unicode_compatible
+class Regency(models.Model):
+    name = models.CharField(max_length=255)
+    regency_id = models.CharField(max_length=80, unique=True)
+    province = models.ForeignKey(Province, on_delete=PROTECT)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, **kwargs):
+        raise NotImplementedError
+
+
+@python_2_unicode_compatible
+class District(models.Model):
+    name = models.CharField(max_length=255)
+    district_id = models.CharField(max_length=80, unique=True)
+    regency = models.ForeignKey(Regency, on_delete=PROTECT)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, **kwargs):
+        raise NotImplementedError
+
+
+@python_2_unicode_compatible
+class Village(models.Model):
+    name = models.CharField(max_length=255)
+    village_id = models.CharField(max_length=80, unique=True)
+    district = models.ForeignKey(District, on_delete=PROTECT)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, **kwargs):
+        raise NotImplementedError
